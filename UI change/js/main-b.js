acbaaa90ce2ede43843615b068003281a91fa170
ZeusLMT <lemanhtuan275@gmail.com>
2017-11-20 15:51:58 +0200

    Finished task 5:upload and task 6:UI change

--- UI change/js/main-b.js
+++ UI change/js/main-b.js
@@ -15,25 +15,38 @@
  */
 // After the loop print the HTML into <ul> element using innerHTML.
 const showImages = () => {
-  const ul = document.querySelector('ul');
+    const ul = document.querySelector('ul');
 
-  fetch('images.json').then((response) => {
-    return response.json();
-  }).then((json) => {
-    let html = '';
-    json.forEach((image) => {
-      html +=
-          `<li>
+    fetch('images.json').then((response) => {
+        return response.json();
+    }).then((json) => {
+        let html = '';
+        json.forEach((image) => {
+            html +=
+                `<li>
           <figure>
-            <a href="img/original/${image.mediaUrl}"><img src="img/thumbs/${image.mediaThumb}"></a>
+            <a href="#" onclick="openModal('img/original/${image.mediaUrl}')"><img src="img/thumbs/${image.mediaThumb}"></a>
             <figcaption>
                 <h3>${image.mediaTitle}</h3>
             </figcaption>
           </figure>
         </li>`;
+        });
+        ul.innerHTML = html;
     });
-    ul.innerHTML = html;
-  });
 };
 
 showImages();
+var myModal = document.getElementById("myModal");
+
+const openModal = (url) => {
+    console.log(url)
+    myModal.style.display = "block";
+    var modalImg = document.getElementById("img");
+    modalImg.src = url;
+};
+
+var span = document.getElementsByClassName("closeBtn")[0];
+span.onclick = function() {
+    myModal.style.display = "none";
+}
